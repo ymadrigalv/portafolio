22f7ca4b46c92929afc2fb8c07f3f2ceae517af7
yuriesky <yurieskymadrigal@gmail.com>
2023-07-26 12:40:27 -0400

    Second version of the Landing

--- js/script.js
+++ js/script.js
@@ -1,63 +1,43 @@
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*= ' + id + ']').classList.add('active');
-            });
-        }
-    });
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-//sticky navbar
-let header = document.querySelector('header');
-header.classList.toggle('sticky', window.scrollY > 100);
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*= " + id + "]")
+          .classList.add("active");
+      });
+      sec.classList.add("show-animate");
+    } else {
+      sec.classList.remove("show-animate");
+    }
+  });
+  menuIcon.classList.remove("bi-x");
+  navbar.classList.remove("active");
 
+  let footer = document.querySelector("footer");
 
-//Remove toggle icon and navbar when click
-menuIcon.classList.remove('bi-x');
-navbar.classList.remove('active');
+  footer.classList.toggle(
+    "show-animate",
+    this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight
+  );
 };
 
+let menuIcon = document.querySelector("#menu-mobile i");
+let navbar = document.querySelector(".navbar");
 
-//Toggle icon nav
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-console.log(navbar);
-
-menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('bi-x');
-    navbar.classList.toggle('active');
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("bi-x");
+  navbar.classList.toggle("active");
 });
-
-
-
-//Scroll Reveal
-ScrollReveal({
-    //reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
-});
-
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
-
-//typed JS
-const typed = new Typed('.multiple-text', {
-    strings: ['FrontEnd Developer', 'Drupal Developer', 'Wordpress Dev', 'Angular Developer'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
-
